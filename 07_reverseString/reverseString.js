6e0cf6a146d9ffb6e273dd12115230bd6351a4e1
BI <197296107+BleachedImago@users.noreply.github.com>
2026-01-01 12:32:04 -0500

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let array = [];
+    for (let i = string.split("").length - 1; i >= 0; i--) {
+        array.push(string.split("")[i]);
+    }
+
+    return array.join("");
 
 };
 
