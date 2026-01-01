ca412e3456e047dcc2afbbc961a6f05456871836
BI <197296107+BleachedImago@users.noreply.github.com>
2026-01-01 12:53:57 -0500

    Finished 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+
+    return array.filter((num) => !(element.includes(num)))
 };
 
 // Do not edit below this line
