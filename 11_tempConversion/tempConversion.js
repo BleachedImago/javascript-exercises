28b0a3b9f9a0a8134db3965b791646452a8f6670
BI <197296107+BleachedImago@users.noreply.github.com>
2026-01-01 22:56:14 -0500

    Finished 11_tempConversoin

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  fah = Number(((fah - 32) * (5 / 9)).toFixed(1));
+
+  if (fah % 1 == 0) {
+    return Math.round(fah);
+  }
+
+  return fah;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  cel = Number((cel * (9 /5 ) + 32).toFixed(1));
+
+  if (cel % 1 == 0) {
+    return Math.round(cel);
+  }
+
+  return cel;
 };
 
 // Do not edit below this line
