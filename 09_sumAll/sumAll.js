e484718a9af5c349bd50e8bdaff3b0bbcfe1c95d
BI <197296107+BleachedImago@users.noreply.github.com>
2026-01-01 22:07:43 -0500

    Finished 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let big;
+    let small;
+
+    if (num1 < 0 || num1 % 1 != 0 || typeof num1 != "number") {
+        return "ERROR"
+    }
+
+    if (num2 < 0 || num2 % 1 != 0 || typeof num2 != "number") {
+        return "ERROR"
+    }
+
+    if (num1 > num2) {
+        big = num1;
+        small = num2;
+    } else {
+        small = num1;
+        big = num2;
+    }
+
+    let sum = 0;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
